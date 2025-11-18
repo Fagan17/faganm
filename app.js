464055780a9bcb370221d00020c5dc6d340eb2e6
Fagan17 <rodantype3@gmail.com>
2025-11-18 22:25:39 +0400

    update

---
+++ app.js
@@ -0,0 +1,8 @@
+let bookPrice = 25;
+let bookAmount = 5;
+console.log(bookPrice * bookAmount);
+let num = 13;
+console.log(Math.pow(num,5));
+console.log(num % 3);
+let randomNumber = Math.floor(Math.random() * (4 + 1)) + 96;
+console.log(randomNumber);

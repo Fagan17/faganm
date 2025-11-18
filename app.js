c5ed86c382dd589dd8847d0c2cce3abdb5064fdd
Fagan17 <rodantype3@gmail.com>
2025-11-18 22:28:45 +0400

    update

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ console.log(bookPrice * bookAmount);
 let num = 13;
 console.log(Math.pow(num,5));
 console.log(num % 3);
-let randomNumber = Math.floor(Math.random() * (4 + 1)) + 96;
+let randomNumber = Math.floor(Math.random() * (4 + 1)) + 95;
 console.log(randomNumber);

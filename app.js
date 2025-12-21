c7b51e0fb0b65c9b1794b80de76b91e391fa6615
Fagan17 <rodantype3@gmail.com>
2025-12-21 20:46:11 +0400

    sas

--- app.js
+++
@@ -1,8 +0,0 @@
-let age = prompt("Yasinizi daxil edin") 
-if(age < 18){
-    console.log("icaze verilmedi ")
-}else{
-    console.log("Icaze verildi")
-}
-let fullName = prompt("Adinizi Soyadinizi daxil edin")
-console.log("Salam,!",fullName.split(" ")[1])
